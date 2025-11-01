9cc73f91e6c2c669480c090160a599668b90890b
nazim32 <chowdhurynazim32@gmail.com>
2025-11-01 21:57:16 +0600

    A new change in the project 5.

--- project-5/script.js
+++ project-5/script.js
@@ -6,7 +6,10 @@ function hurtRain()
 
        container.classList.add('hurtContainer');
        container.style.left = Math.random()*100 +'vw';
-       container.style.animationDuration = Math.random()*6;
+       container.style.animationDuration = Math.random()*6 + 's';
+       setTimeout(()=>{
+              container.remove();
+       }, 3000);
 }
 
 setInterval(hurtRain, 100);
